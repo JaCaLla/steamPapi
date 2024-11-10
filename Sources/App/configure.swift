341093b3091b4727be744d71b94f89fb62788e98
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2024-11-10 14:50:52 +0100

    [configuration management] Setup DDBB name, and pick up credentials from .env file

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -3,17 +3,27 @@ import Fluent
 import FluentPostgresDriver
 import Vapor
 
+private struct MissingDatabaseCredentials: Error {}
+
 // configures your application
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+    guard
+        let dbUser = Environment.get("DATABASE_USERNAME"),
+        let dbPass = Environment.get("DATABASE_PASSWORD"),
+        let dbName = Environment.get("DATABASE_NAME")
+    else {
+        throw MissingDatabaseCredentials()
+    }
+
     app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database",
+        username: dbUser,
+        password: dbPass,
+        database: dbName,
         tls: .prefer(try .init(configuration: .clientDefault)))
     ), as: .psql)
 
